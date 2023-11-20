8588f3cb236855e6da4851af53ccf97cb63a4a22
Sandboks <sandboxasdf@gmail.com>
2023-11-20 18:01:31 +1100

    04 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    for (let i = 1; i < arguments.length; i++) {
+        let removeMe = arguments[i]
+        while (array.indexOf(removeMe) != -1) {
+            array.splice(array.indexOf(removeMe), 1)
+        }
+    }
+    
 
+    return array
 };
 
 // Do not edit below this line
