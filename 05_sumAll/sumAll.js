bb037cdaa0c6ea0975e526319496f7fd034b8cf2
Sandboks <sandboxasdf@gmail.com>
2023-11-20 18:14:28 +1100

    05 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!(a <= b)) {
+        let c = a
+        a = b
+        b = c
+    }
 
+    if ((a < 0) || (b < 0) || !Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+    
+    let sum = 0
+    for (let i = a; i <= b; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
