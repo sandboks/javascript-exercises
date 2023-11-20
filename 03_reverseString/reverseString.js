3c095f8680bf964f9211e0f023416153bfd8a344
Sandboks <sandboxasdf@gmail.com>
2023-11-20 15:57:06 +1100

    solved 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let reverse = ""
 
+    for (let i = s.length; i >= 0; i--) {
+        reverse += s.charAt(i)
+    }
+
+    return reverse
 };
 
 // Do not edit below this line
