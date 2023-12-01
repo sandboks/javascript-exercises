d9ffa4f5c159b037f035224c5509ff052af2ee4d
Sandboks <sandboxasdf@gmail.com>
2023-12-01 21:00:00 +1100

    08 solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
 const add = function() {
-	
+	let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
+const sum = function(array) {
 	
+	return array.reduce((product, i) =>  product + i, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((product, i) =>  product * i)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let f = 1;
+  while (n > 0) {
+    f *= n;
+    n--;
+  }
+  return f;
 };
 
 // Do not edit below this line
