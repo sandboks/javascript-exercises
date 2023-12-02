7a385619f70d3576a69ffb5457114af7bfdcaecd
Sandboks <sandboxasdf@gmail.com>
2023-12-02 21:21:27 +1100

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    //0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    if (n < 0)
+        return "OOPS";
+    if (n <= 2)
+        return 1;
+    
+    return fibonacci(n-1) + fibonacci(n-2);
 };
 
 // Do not edit below this line
