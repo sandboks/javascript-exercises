ba780f7f88c2af7887365ce3eea91dd40cc5c567
Sandboks <sandboxasdf@gmail.com>
2023-12-02 21:10:54 +1100

    09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.replace(/[^a-z0-9]/gi, ''); // get alphanumeric only
+    s = s.toUpperCase();
 
+    let a = s.slice(0, s.length/2);
+    let b = s.slice(Math.round((s.length/2)), s.length);
+    b = b.split("").reduce((acc, char) => char + acc, ""); // reverse the string
+
+    //return (a + "/" + b);
+
+    return (a == b);
+
+    return b;
+
+    return s;
 };
 
 // Do not edit below this line
