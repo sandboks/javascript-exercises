ea3e901d2944a22d9bf16e0b191e391d1a05e93a
Sandboks <sandboxasdf@gmail.com>
2023-12-04 16:38:31 +1100

    solutions completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -5,7 +5,20 @@ const fibonacci = function(n) {
     if (n <= 2)
         return 1;
     
-    return fibonacci(n-1) + fibonacci(n-2);
+    //return fibonacci(n-1) + fibonacci(n-2);
+
+    let prev = 1; // (n-1)
+    let prevprev = 1; // (n-2)
+    let current = 2; // n
+
+    for (let i = 3; i < n; i++) {
+        prevprev = prev;
+        prev = current;
+        current = prev + prevprev;
+    }
+
+    return current;
+
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.sort(function(a, b) {
 
+        let ageA = ((a.yearOfDeath === undefined) ? 2023 : a.yearOfDeath) - a.yearOfBirth;
+        let ageB = ((b.yearOfDeath === undefined) ? 2023 : b.yearOfDeath) - b.yearOfBirth;
+
+        return ageA - ageB;
+    })[array.length - 1];
 };
 
 // Do not edit below this line
